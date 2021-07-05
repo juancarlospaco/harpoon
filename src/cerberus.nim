dba3148aa9894f45f7fd0628e853858b84a16482
Juan Carlos <juancarlospaco@gmail.com>
2021-07-05 15:40:07 -0300

    Unroll string ops

--- src/cerberus.nim
+++ src/cerberus.nim
@@ -1,4 +1,4 @@
-include prelude # TODO: imports
+import std/[os, net, uri, base64, macros, parseutils, strutils, httpcore, importutils]
 
 const cacertUrl* = "https://curl.se/ca/cacert.pem"  # cacert.pem URL.
 
@@ -7,6 +7,14 @@ template newDefaultHeaders*(body = ""; userAgent = "x"; contentType = "text/plai
 
 template newDefaultHeaders*(body = ""; userAgent = "x"; contentType = "text/plain"; accept = "*/*"; proxyUser, proxyPassword: string): array[5, (string, string)] =
   [("Content-Length", $body.len), ("User-Agent", userAgent), ("Content-Type", contentType), ("Accept", accept), ("Proxy-Authorization", "Basic " & base64.encode(proxyUser & ' ' & proxyPassword))]
+
+macro unrollStringOps(x: ForLoopStmt) =
+  expectKind x, nnkForStmt
+  var body = newStmtList()
+  for chara in x[^2][^2].strVal:
+    body.add nnkAsgn.newTree(x[^2][^1], chara.newLit)
+    body.add x[^1]
+  result = body
 
 template parseHttpCode(s: string): int =
   template char2Int(c: '0'..'9'; pos: static int): int =
@@ -22,3 +30,41 @@ template parseHttpCode(s: string): int =
     of '9': static(9 * pos)
     else:   0
   char2Int(s[9], 100) + char2Int(s[10], 10) + char2Int(s[11], 1)
+
+func toString*(url: Uri; metod: HttpMethod; headers: openArray[(string, string)]; body: string): string =
+  var it: char
+  var path = url.path
+  if path.len == 0: path = "/"
+  if url.query.len > 0:
+    path.add '?'
+    path.add url.query
+  case metod
+  of HttpGet:
+    for _ in unrollStringOps("GET ", it):     result.add it
+  of HttpPost:
+    for _ in unrollStringOps("POST ", it):    result.add it
+  of HttpPut:
+    for _ in unrollStringOps("PUT ", it):     result.add it
+  of HttpHead:
+    for _ in unrollStringOps("HEAD ", it):    result.add it
+  of HttpDelete:
+    for _ in unrollStringOps("DELETE ", it):  result.add it
+  of HttpPatch:
+    for _ in unrollStringOps("PATCH ", it):   result.add it
+  of HttpTrace:
+    for _ in unrollStringOps("TRACE ", it):   result.add it
+  of HttpOptions:
+    for _ in unrollStringOps("OPTIONS ", it): result.add it
+  of HttpConnect:
+    for _ in unrollStringOps("CONNECT ", it): result.add it
+  result.add path
+  for _ in unrollStringOps(" HTTP/1.1\r\nHost: ", it): result.add it
+  result.add url.hostname
+  for _ in unrollStringOps("\r\n", it): result.add it
+  for header in headers:  # Headers have a soft limit of 10_000 char ?.
+    result.add header[0]
+    for _ in unrollStringOps(": ", it): result.add it
+    result.add header[1]
+    for _ in unrollStringOps("\r\n", it): result.add it
+  for _ in unrollStringOps("\r\n", it): result.add it
+  result.add body
