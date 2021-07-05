787bdd20e8f7ca8943c6a798277a37407a5be1fb
Juan Carlos <juancarlospaco@gmail.com>
2021-07-05 15:33:39 -0300

    No one reads commit messages anyway...

--- src/cerberus.nim
+++ src/cerberus.nim
@@ -1,1 +1,9 @@
-echo 42
+import base64
+
+const cacertUrl* = "https://curl.se/ca/cacert.pem"  # cacert.pem URL.
+
+template newDefaultHeaders*(body = ""; userAgent = "x"; contentType = "text/plain"; accept = "*/*"): array[4, (string, string)] =
+  [("Content-Length", $body.len), ("User-Agent", userAgent), ("Content-Type", contentType), ("Accept", accept)]
+
+template newDefaultHeaders*(body = ""; userAgent = "x"; contentType = "text/plain"; accept = "*/*"; proxyUser, proxyPassword: string): array[5, (string, string)] =
+  [("Content-Length", $body.len), ("User-Agent", userAgent), ("Content-Type", contentType), ("Accept", accept), ("Proxy-Authorization", "Basic " & base64.encode(proxyUser & ' ' & proxyPassword))]
