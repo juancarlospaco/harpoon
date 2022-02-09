9efebc8d7b67a00cbf88b53ffb60303da8c42d56
Juan Carlos <juancarlospaco@gmail.com>
2022-02-09 08:16:57 -0800

    .

--- mini/harpoon.nim
+++ mini/harpoon.nim
@@ -1,119 +1,110 @@
 import std/[net,  macros, strutils, times, json, httpcore, importutils, parseutils, uri]
-export HttpMethod, HttpCode
 
-template newDefaultHeaders*(body: string; contentType = "text/plain"; accept = "*/*"; userAgent = "x"): array[5, (string, string)] =
-  [("Content-Length", $body.len), ("User-Agent", userAgent), ("Content-Type", contentType), ("Accept", accept), ("Dnt", "1")]
-
-macro unrollStringOps(x: ForLoopStmt) =
-  expectKind x, nnkForStmt
-  var body = newStmtList()
-  for chara in x[^2][^2].strVal:
-    body.add nnkAsgn.newTree(x[^2][^1], chara.newLit)
-    body.add x[^1]
-  result = body
-
-template parseHttpCode(s: string): int =
-  when defined(danger):
-    template char2Int(c: '0'..'9'; pos: static int): int =
-      case c
-      of '1': static(1 * pos)
-      of '2': static(2 * pos)
-      of '3': static(3 * pos)
-      of '4': static(4 * pos)
-      of '5': static(5 * pos)
-      of '6': static(6 * pos)
-      of '7': static(7 * pos)
-      of '8': static(8 * pos)
-      of '9': static(9 * pos)
-      else:   0
-    char2Int(s[9], 100) + char2Int(s[10], 10) + char2Int(s[11], 1)
-  else:
-    parseInt(s[9..11])
-
-func parseHeaders(data: string): seq[(string, string)] {.inline, raises: [].} =
-  var i = 0
-  while data[i] != '\l': inc i
-  inc i
-  var value = false
-  var current: (string, string)
-  while i < data.len:
-    case data[i]
-    of ':':
-      if value: current[1].add ':'
-      value = true
-    of ' ':
-      if value:
-        if current[1].len != 0: current[1].add data[i]
-      else: current[0].add(data[i])
-    of '\c': discard
-    of '\l':
-      if current[0].len == 0: return result
-      result.add current
-      value = false
-      current = ("", "")
-    else:
-      if value: current[1].add data[i] else: current[0].add data[i]
-    inc i
-  return
-
-func toString(url: Uri; metod: static[HttpMethod]; headers: openArray[(string, string)]; body: string): string {.raises: [].} =
-  var it: char  # TODO: Better name for this func.
-  var temp: string = url.path
-  if unlikely(temp.len == 0): temp = "/"
-  if url.query.len > 0:
-    temp.add '?'
-    temp.add url.query
-  when metod == HttpGet:
-    for _ in unrollStringOps("GET ", it):     result.add it
-  elif metod == HttpPost:
-    for _ in unrollStringOps("POST ", it):    result.add it
-  elif metod == HttpPut:
-    for _ in unrollStringOps("PUT ", it):     result.add it
-  elif metod == HttpHead:
-    for _ in unrollStringOps("HEAD ", it):    result.add it
-  elif metod == HttpDelete:
-    for _ in unrollStringOps("DELETE ", it):  result.add it
-  elif metod == HttpPatch:
-    for _ in unrollStringOps("PATCH ", it):   result.add it
-  elif metod == HttpTrace:
-    for _ in unrollStringOps("TRACE ", it):   result.add it
-  elif metod == HttpOptions:
-    for _ in unrollStringOps("OPTIONS ", it): result.add it
-  elif metod == HttpConnect:
-    for _ in unrollStringOps("CONNECT ", it): result.add it
-  result.add temp
-  for _ in unrollStringOps(" HTTP/1.1\r\nHost: ", it): result.add it
-  result.add url.hostname
-  for _ in unrollStringOps("\r\n", it): result.add it
-  temp.setLen 0  # Reuse variable.
-  for header in headers:
-    temp.add header[0]
-    for _ in unrollStringOps(": ", it):   temp.add it
-    temp.add header[1]
-    for _ in unrollStringOps("\r\n", it): temp.add it
-  for _ in unrollStringOps("\r\n", it):   temp.add it
-  assert not(temp.len > 10_000), "Header must not be > 10_000 char"
-  result.add temp
-  result.add body
-
-proc fetch*(socket: Socket; url: Uri; metod: static[HttpMethod]; headers: openArray[(string, string)]; body = "";
-    timeout = -1; port: static[Port] = 80.Port; portSsl: static[Port] = 443.Port;
+proc fetch*(socket: Socket; url: Uri; metod: static[HttpMethod];
+    headers: openArray[(string, string)] = [("User-Agent", "x"), ("Content-Type", "text/plain"), ("Accept", "*/*"), ("Dnt", "1")];
+    body = ""; timeout = -1; port: static[Port] = 80.Port; portSsl: static[Port] = 443.Port;
     parseHeader = true; parseStatus = true; parseBody = true; bodyOnly: static[bool] = false): auto =
-  assert timeout > -2 and timeout != 0, "Timeout argument must be -1 or a non-zero positive integer"
   var
     res: string
     chunked: bool
     contentLength: int
     chunks: seq[string]
-  let proxi: string = url.hostname
+
+  template parseHttpCode(s: string): int {.used.} =
+    when defined(danger):
+      template char2Int(c: '0'..'9'; pos: static int): int =
+        case c
+        of '1': static(1 * pos)
+        of '2': static(2 * pos)
+        of '3': static(3 * pos)
+        of '4': static(4 * pos)
+        of '5': static(5 * pos)
+        of '6': static(6 * pos)
+        of '7': static(7 * pos)
+        of '8': static(8 * pos)
+        of '9': static(9 * pos)
+        else:   0
+      char2Int(s[9], 100) + char2Int(s[10], 10) + char2Int(s[11], 1)
+    else: parseInt(s[9..11])
+
+  func parseHeaders(data: string): seq[(string, string)] {.inline, used, raises: [].} =
+    var i = 0
+    while data[i] != '\l': inc i
+    inc i
+    var value = false
+    var current: (string, string)
+    while i < data.len:
+      case data[i]
+      of ':':
+        if value: current[1].add ':'
+        value = true
+      of ' ':
+        if value:
+          if current[1].len != 0: current[1].add data[i]
+        else: current[0].add(data[i])
+      of '\c': discard
+      of '\l':
+        if current[0].len == 0: return result
+        result.add current
+        value = false
+        current = ("", "")
+      else:
+        if value: current[1].add data[i] else: current[0].add data[i]
+      inc i
+    return
+
+  func toString(url: Uri; metod: static[HttpMethod]; headers: openArray[(string, string)]; body: string): string {.raises: [].} =
+    var it: char
+    var temp: string = url.path
+    macro unrollStringOps(x: ForLoopStmt) =
+      result = newStmtList()
+      for chara in x[^2][^2].strVal:
+        result.add nnkAsgn.newTree(x[^2][^1], chara.newLit)
+        result.add x[^1]
+    if unlikely(temp.len == 0): temp = "/"
+    if url.query.len > 0:
+      temp.add '?'
+      temp.add url.query
+    when metod == HttpGet:
+      for _ in unrollStringOps("GET ", it):     result.add it
+    elif metod == HttpPost:
+      for _ in unrollStringOps("POST ", it):    result.add it
+    elif metod == HttpPut:
+      for _ in unrollStringOps("PUT ", it):     result.add it
+    elif metod == HttpHead:
+      for _ in unrollStringOps("HEAD ", it):    result.add it
+    elif metod == HttpDelete:
+      for _ in unrollStringOps("DELETE ", it):  result.add it
+    elif metod == HttpPatch:
+      for _ in unrollStringOps("PATCH ", it):   result.add it
+    elif metod == HttpTrace:
+      for _ in unrollStringOps("TRACE ", it):   result.add it
+    elif metod == HttpOptions:
+      for _ in unrollStringOps("OPTIONS ", it): result.add it
+    elif metod == HttpConnect:
+      for _ in unrollStringOps("CONNECT ", it): result.add it
+    result.add temp
+    for _ in unrollStringOps(" HTTP/1.1\r\nHost: ", it): result.add it
+    result.add url.hostname
+    for _ in unrollStringOps("\r\n", it): result.add it
+    temp.setLen 0
+    for header in headers:
+      temp.add header[0]
+      for _ in unrollStringOps(": ", it):   temp.add it
+      temp.add header[1]
+      for _ in unrollStringOps("\r\n", it): temp.add it
+    for _ in unrollStringOps("\r\n", it):   temp.add it
+    result.add temp
+    result.add body
+
   if likely(url.scheme == "https"):
     var ctx =
       try:    newContext(verifyMode = CVerifyNone)
       except: raise newException(IOError, getCurrentExceptionMsg())
-    socket.connect(proxi, portSsl, timeout)
-    try:    ctx.wrapConnectedSocket(socket, handshakeAsClient, proxi)
+    socket.connect(url.hostname, portSsl, timeout)
+    try:    ctx.wrapConnectedSocket(socket, handshakeAsClient, url.hostname)
     except: raise newException(IOError, getCurrentExceptionMsg())
-  else: socket.connect(proxi, port, timeout)
+  else: socket.connect(url.hostname, port, timeout)
   socket.send(toString(url, metod, headers, body))
   while true:
     let line = socket.recvLine(timeout)
@@ -151,34 +142,15 @@ proc fetch*(socket: Socket; url: Uri; metod: static[HttpMethod]; headers: openAr
     chunks.add chunk
   when bodyOnly: result = chunks
   else:
-    privateAccess url.type  # To use Uri.isIpv6
+    privateAccess url.type
     result = (url: url, metod: metod, isIpv6: url.isIpv6,
               headers: if parseHeader: parseHeaders(res)           else: @[],
               code:    if parseStatus: parseHttpCode(res).HttpCode else: 0.HttpCode,
               body:    if parseBody:   chunks                      else: @[])
 
-template fetchImpl(code, result): untyped {.dirty.} =
-  let socket: Socket = newSocket()
-  try: result = code
-  finally: close socket
-
-proc get*(url: Uri): auto =
-  fetchImpl(socket.fetch(url, HttpGet, newDefaultHeaders""), result)
-
-proc getContent*(url: Uri): seq[string] =
-  fetchImpl(socket.fetch(url, HttpGet, newDefaultHeaders"", bodyOnly = true), result)
-
-proc post*(url: Uri; body: string): auto =
-  fetchImpl(socket.fetch(url, HttpPost, newDefaultHeaders(body), body), result)
-
-proc postContent*(url: Uri; body: string): seq[string] =
-  fetchImpl(socket.fetch(url, HttpPost, newDefaultHeaders(body), body, bodyOnly = true), result)
 
 runnableExamples"--gc:orc --experimental:strictFuncs -d:ssl -d:nimStressOrc --import:std/httpcore":
-  from std/uri import parseUri  # To use Uri.
-  block:
-    doAssert get(parseUri"http://httpbin.org/get").code == Http200
-    doAssert getContent(parseUri"http://httpbin.org/get").len > 0
-  block:
-    doAssert post(parseUri"http://httpbin.org/post", "data here").code == Http200
-    doAssert postContent(parseUri"http://httpbin.org/post", "data here").len > 0
+  import std/[net, uri]
+  let socket: Socket = newSocket()
+  doAssert socket.fetch(parseUri"http://httpbin.org/post", HttpPost, body = "data here").code == Http200
+  close socket
